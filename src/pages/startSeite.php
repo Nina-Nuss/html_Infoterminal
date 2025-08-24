c5f222e23f2deaeafaf00ade0c58f0d64e23aa5d
nina.nuss <nina.nuss01@gmail.com>
2025-08-24 19:08:43 +0200

    .

--- src/pages/startSeite.php
+++ src/pages/startSeite.php
@@ -16,13 +16,13 @@
             <?php include $_SERVER['DOCUMENT_ROOT'] . '/src/layout/sidebar.php'; ?>
             <div class="col-md-10 text-center">
                 <?php include $_SERVER['DOCUMENT_ROOT'] . '/src/layout/selectPanel.php'; ?>
-                <hr>    
+                <hr>
                 <div class="d-flex justify-content-center">
-                        <button id="btn_save_changes" type="button" onclick="CardObj.saveChanges()"
-                            class="btn btn-success btn-sm me-2">
-                            <i class="fas fa-save"></i> Speichern
-                        </button>
-                   <!--  -->
+                    <button id="btn_save_changes" type="button" onclick="CardObj.saveChanges()"
+                        class="btn btn-success btn-sm me-2">
+                        <i class="fas fa-save"></i> Speichern
+                    </button>
+                    <!--  -->
                     <button id="btn_addInfoSeite" type="button" class="btn btn-success btn-sm me-2"
                         data-bs-toggle="modal" data-bs-target="#addInfoSeite">
                         <i class="fas fa-pen-to-square"></i> Erstellen
@@ -35,18 +35,17 @@
                 <div class="pt-2"></div>
                 <div class="card-body h-100">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-9">
                             <div class="card h-100">
                                 <div class="card-header p-0">
-                                    <small class="mb-0"><i class="fas fa-cog me-2"></i> Infoseite Eigenschaften</small>
+                                    <small class="mb-0"><i class="fas fa-cog me-2"></i> Infoseite</small>
                                 </div>
 
-
-                                <div class="d-flex justify-content-between">
-                                    <div class="card-body">
+                                <div class="d-flex justify-content-center">
+                                    <div class="card-body" >
                                         <div class="form-group mt-0">
                                             <div class="d-flex align-items-center justify-content-start mb-3">
-                                                <label for="websiteName" class="form-label mb-0 w-50 ">Infoseite Name:
+                                                <label for="websiteName" class="form-label mb-0 ">Infoseite Name:
                                                 </label>
                                                 <input type="text" class="form-control  w-50 ml-0 form-control-sm"
                                                     id="websiteName" value="Infoseite auswählen" readonly>
@@ -55,31 +54,31 @@
 
                                         <div class="form-group">
                                             <div class="d-flex align-items-center  justify-content-start mb-2 ">
-                                                <label for="anzeigeDauer" class="form-label mb-0  w-50">
+                                                <label for="anzeigeDauer" class="form-label mb-0 ">
                                                     Anzeige Dauer:</label>
                                                 <input class="form-control w-50  form-control-sm " id="anzeigeDauer"
                                                     value="Infoseite auswählen" readonly></input>
                                             </div>
-                                            <div class="d-flex align-items-center justify-content-end gap-2">
+                                            <div class="d-flex align-items-center justify-content-center gap-2">
                                                 <i class="fas fa-arrow-right"></i>
+
+
                                                 <div for="selectSekunden">Sekunden:</div>
                                                 <input type="text" id="selectSekunden"
                                                     class="form-control form-control-sm ml-2" maxlength="4"
                                                     style="width: 70px;">
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-center gap-2 w-50">
                                                 <input class="form-check-input" type="checkbox" id="checkA"
                                                     name="checkA" onchange="CardObj.checkAktiv()">
                                                 <label class="form-check-label mb-0" for="checkA">
                                                     anzeigen
                                                 </label>
+
                                             </div>
+                                           
                                         </div>
                                     </div>
-                                    <div class="card-body w-75">
-
-
-                                        <div class="d-flex align-items-center justify-content-center gap-2 mb-2">
+                                    <div class="card-body w-75" >
+                                        <div class="d-flex align-items-center justify-content-between gap-2 mb-2">
                                             <div class="btn-group   " id="btnShowZeitraum" role="group"
                                                 aria-label="Zeit Konfiguration" style="width: 200px;">
                                                 <button type="button" class="btn btn-sm btn-custom-outline"
@@ -91,12 +90,18 @@
                                                 <button type="button" class="btn btn-sm btn-custom-outline"
                                                     onclick="showDateTime('uhrzeit')"><i
                                                         class="fas fa-clock me-2"></i></button>
+                                            </div>
+                                            <div class=" d-flex align-items-center">
+                                                <select class="form-control form-select-sm" id="infotherminalSelect">
+                                                    <option value="">-- Bitte wählen --</option>
+                                                </select>
+                                                <button id="openTerminalBtn" class="btn text-dark start-btn btn-sm"
+                                                    style="border-color: #006c99;  background-color: rgba(255, 255, 255, 0.952);">
+                                                    <i class="fas fa-external-link-alt me-1"></i>öffnen
+                                                </button>
 
                                             </div>
-
-
                                         </div>
-
                                         <div class="d-flex">
                                             <div id="panelForDateTime" class="w-100">
                                                 <div id="dateTimeInfoPanel"
@@ -212,28 +217,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-3">
-                           
 
-                            <div class="card">
-                                <div class="card-header p-0">
-                                    <small class="mb-0"><i class="fas fa-desktop me-2"></i>Testanzeige</small>
-                                </div>
-                                <div class="card-body">
-                                    <div class=" g-2 d-flex align-items-center justify-content-center">
-                                        <select class="form-control form-select-sm w-75 " id="infotherminalSelect">
-                                            <option value="">-- Bitte wählen --</option>
-                                        </select>
-                                        <button id="openTerminalBtn" class="btn text-dark start-btn btn-sm w-25"
-                                            style="border-color: #006c99;  background-color: rgba(255, 255, 255, 0.952);">
-                                            <i class="fas fa-external-link-alt me-1"></i>öffnen
-                                        </button>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
                         <div class="col-md-3">
                             <div class="card h-100">
                                 <div class="card-header p-0">
