9d93c151eaf9b7c75de082c2addfe29c1934aa46
nina.nuss <nina.nuss01@gmail.com>
2025-09-12 21:40:24 +0200

    .

--- src/layout/logo.php
+++ src/layout/logo.php
@@ -1,8 +1,8 @@
 
 <div id="header" class=" d-flex p-0" style="background-color: white; height: 11vh; margin-left: 1%;">
-    <div class="d-flex">
+    <div class="d-flex" style="margin: 2px;">
         <img src="../images/bild.png" alt="bild"
-           class="mx-4 rounded" style="border-radius: 2vh; margin: 2px;">
+           class="mx-4 rounded" style="border-radius: 2vh;">
         <img src="../images/logo.png" class="m-1" alt="logo" style="width: 20vh; align-self: start;">
     </div>
     <link rel="stylesheet" href="../css/index_new.css">
